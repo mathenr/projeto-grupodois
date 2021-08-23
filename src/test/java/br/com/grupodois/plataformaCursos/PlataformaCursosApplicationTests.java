4f064c355d96f91b066ef411fd885da7092d1eb7
Patrick Mota <patrickvieiramota@gmail.com>
2021-08-23 11:36:43 -0300

    First commit

---
+++ src/test/java/br/com/grupodois/plataformaCursos/PlataformaCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.grupodois.plataformaCursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
