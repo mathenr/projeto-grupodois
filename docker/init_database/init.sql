2eb1d29f34a07bf1d41a179658893e89a09bf3d0
Matheus Mendes <matheushenriquesad@gmail.com>
2021-08-24 11:17:13 -0300

    init sql privileges

--- docker/init_database/init.sql
+++ docker/init_database/init.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE plataforma_cursos_db;
 CREATE USER 'plataformacursos'@'%' IDENTIFIED BY 'plataforma%';
-GRANT SELECT ON *.* TO 'plataformacursos'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'plataformacursos'@'%';
 FLUSH PRIVILEGES;
