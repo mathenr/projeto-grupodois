c59d899247f474bbb1286caf54f0cd3390aada35
Matheus Mendes <matheushenriquesad@gmail.com>
2021-08-26 16:42:02 -0300

    feat(course): "Folders changed, and course service added"

--- src/main/java/br/com/grupodois/plataformaCursos/repository/CourseRepository.java
+++ src/main/java/br/com/grupodois/plataformaCursos/repository/CourseRepository.java
@@ -1,6 +1,6 @@
 package br.com.grupodois.plataformaCursos.repository;
 
-import br.com.grupodois.plataformaCursos.modelo.Course;
+import br.com.grupodois.plataformaCursos.model.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
