6d7fb30372ca2fee2cec3134818f957e9a231289
Patrick Mota <patrick.mota@invillia.com>
2021-08-26 10:11:32 -0300

    feat(course): add cache

--- src/main/java/br/com/grupodois/plataformaCursos/controller/CourseController.java
+++ src/main/java/br/com/grupodois/plataformaCursos/controller/CourseController.java
@@ -6,6 +6,8 @@ import br.com.grupodois.plataformaCursos.dto.form.course.CourseUpdateForm;
 import br.com.grupodois.plataformaCursos.model.Course;
 import br.com.grupodois.plataformaCursos.repository.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -27,6 +29,7 @@ public class CourseController {
     private CourseRepository courseRepository;
 
     @GetMapping
+    @Cacheable(value = "courses")
     public Page<CourseDto> findAll(@PageableDefault(sort = "id", direction = Sort.Direction.ASC, page = 0)
                                                Pageable pagination){
         Page<Course> courses = courseRepository.findAll(pagination);
@@ -34,17 +37,8 @@ public class CourseController {
         return CourseDto.convert(courses);
     }
 
-    @PostMapping
-    @Transactional
-    public ResponseEntity<CourseDto> store(@RequestBody @Valid CourseForm courseForm, UriComponentsBuilder uriBuilder){
-        Course course = courseForm.convert();
-        courseRepository.save(course);
-
-        URI uri = uriBuilder.path("/courses/{id}").buildAndExpand(course.getId()).toUri();
-        return ResponseEntity.created(uri).body(new CourseDto(course));
-    }
-
     @GetMapping("/{id}")
+    @Cacheable(value = "course")
     public ResponseEntity<CourseDto> findOne(@PathVariable Long id){
         Optional<Course> course = courseRepository.findById(id);
         if(course.isPresent()){
@@ -53,8 +47,20 @@ public class CourseController {
         return ResponseEntity.notFound().build();
     }
 
+    @PostMapping
+    @Transactional
+    @CacheEvict(value = {"courses", "course"}, allEntries = true)
+    public ResponseEntity<CourseDto> store(@RequestBody @Valid CourseForm courseForm, UriComponentsBuilder uriBuilder){
+        Course course = courseForm.convert();
+        courseRepository.save(course);
+
+        URI uri = uriBuilder.path("/courses/{id}").buildAndExpand(course.getId()).toUri();
+        return ResponseEntity.created(uri).body(new CourseDto(course));
+    }
+
     @PutMapping("/{id}")
     @Transactional
+    @CacheEvict(value = {"courses", "course"}, allEntries = true)
     public ResponseEntity<CourseDto> update(@PathVariable Long id, @RequestBody @Valid CourseUpdateForm courseUpdateForm){
         Optional<Course> optional = courseRepository.findById(id);
         if (optional.isPresent()){
@@ -66,6 +72,7 @@ public class CourseController {
 
     @DeleteMapping("/{id}")
     @Transactional
+    @CacheEvict(value = {"courses", "course"}, allEntries = true)
     public ResponseEntity<?> delete(@PathVariable Long id){
         Optional<Course> course = courseRepository.findById(id);
         if(course.isPresent()){
